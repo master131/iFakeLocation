4e42a2d675d0dd1d792253907e4d1ded2da558bc
master131 <master131_@live.com>
2019-06-11 09:30:56 +1000

    Auto-detect latest iOS developer images. Bumped version to 1.2.

--- iFakeLocation/DeveloperImageHelper.cs
+++ iFakeLocation/DeveloperImageHelper.cs
@@ -31,7 +31,6 @@ namespace iFakeLocation
 
         private static readonly Dictionary<string, string> VersionMapping = new Dictionary<string, string>
         {
-            {"12.3", "12.2"}
         };
 
         public static string GetSoftwareVersion(DeviceInformation device)
@@ -57,9 +56,22 @@ namespace iFakeLocation
             // Populate URLs for developer images from Github
             if (VersionToImageUrl.Count == 0)
             {
+                string treeList = "795fc91f28cb3884edc45b876482911c797de85c";
+                try
+                {
+                    WebClient.Headers["X-Requested-With"] = "XMLHttpRequest";
+                    var resp = WebClient.DownloadString("https://github.com/xushuduo/Xcode-iOS-Developer-Disk-Image/find/master?_pjax=%23js-repo-pjax-container");
+                    var tl = "/tree-list/";
+                    var idx = resp.IndexOf(tl, StringComparison.InvariantCultureIgnoreCase);
+                    if (idx != -1)
+                        treeList = resp.Substring(idx + tl.Length, resp.IndexOf('\"', idx) - (idx + tl.Length));
+                }
+                catch
+                {
+                }
                 WebClient.Headers["Accept"] = "application/json";
                 WebClient.Headers["X-Requested-With"] = "XMLHttpRequest";
-                var response = WebClient.DownloadString("https://github.com/xushuduo/Xcode-iOS-Developer-Disk-Image/tree-list/6718c0ced4dadf28ecc8411fddce32f4f779db9f");
+                var response = WebClient.DownloadString("https://github.com/xushuduo/Xcode-iOS-Developer-Disk-Image/tree-list/" + treeList);
                 var paths = response.Split('"').Where(s => s.EndsWith(".dmg", StringComparison.InvariantCultureIgnoreCase)).ToArray();
                 foreach (var path in paths)
                     VersionToImageUrl.Add(path.Split('/')[1].Split(' ')[0], "https://github.com/xushuduo/Xcode-iOS-Developer-Disk-Image/raw/master/" + path);
