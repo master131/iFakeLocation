f7016fab374188eba1f1d43db164d33a17cd3b12
master131 <master131_@live.com>
2023-09-29 17:47:50 +1000

    Fix library resolution on .NET Core for libplist

--- iFakeLocation/NativeMethods.cs
+++ iFakeLocation/NativeMethods.cs
@@ -1,10 +1,27 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using iMobileDevice.Plist;
 
 namespace iFakeLocation
 {
     internal static class NativeMethods
     {
+        static NativeMethods() {
+            EnsureRegistered();
+        }
+
+        private static void EnsureRegistered() {
+#if !NETCOREAPP2_0 && !NETSTANDARD2_0 && !NET45
+            // Special glue which fixes the DllImport library name and resolves the library handle using
+            // existing logic in iMobileDevice-net (have to resort to reflection as its internal)
+            NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(),
+                (DllImportResolver) Delegate.CreateDelegate(typeof(DllImportResolver),
+                    typeof(PlistNativeMethods).Assembly.GetType("iMobileDevice.LibraryResolver")
+                        .GetMethod("DllImportResolver", BindingFlags.NonPublic | BindingFlags.Static)));
+#endif
+        }
+        
         [DllImport(PlistNativeMethods.LibraryName, EntryPoint="plist_new_data", CallingConvention=CallingConvention.Cdecl)]
         private static extern unsafe PlistHandle plist_new_data(byte* val, ulong length);
 
